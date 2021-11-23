da46678dfe1d48434ea4afa0c39b6aa5c988199f
miletic94 <mileticnemanja.lab@gmail.com>
2021-11-23 14:29:51 +0100

    Server updates and fix MIME type not supported
    - server updates
     - update server routes to have api/ as prefix
     - add server route for invoice details
    
    - move style from style.css in ./public/style.css to ./src/index.css
    to fix "MIME type not allowed bug"

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,17 @@ const path = require("path")
 
 app.use("/assets", express.static("assets"))
 
-app.get("/invoices", (req, res) => {
+app.get("/api/invoices", (req, res) => {
     res.send(data)
+})
+
+app.get("/api/invoices/:id", (req, res) => {
+    const response = data.find(item => item.id === req.params.id)
+    if(response) {
+        res.send(response)
+        return
+    } 
+    res.status(404).send("Invoice not found.")
 })
 
 const PORT = process.env.PORT || 5000
